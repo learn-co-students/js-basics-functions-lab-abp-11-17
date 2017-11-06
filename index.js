66ef5e9ebab3c014e2cb16cbd5cab0e05b1e401d
jencekade <gaephuoc@gmail.com>
2017-11-06 00:49:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  var distance = Math.abs(pickupLocation - 42);
+  return distance;
+}
+
+function distanceFromHqInFeet() {
+
+
+}
+
+function distanceTravelledInFeet() {
+
+
+}
+
+function calculatesFarePrice(start, destination) {
+
+
+}
