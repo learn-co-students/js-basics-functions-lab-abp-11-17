d0a29e8a22c9cb08bc5c19a956bc972673879601
harenarium <carolinel2610@gmail.com>
2017-11-09 00:52:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock){
+  const blocksHQ = Math.abs(pickupBlock-42);
+  return blocksHQ;
+}
+
+
+function distanceFromHqInFeet(pickupBlock){
+  const feetHQ = distanceFromHqInBlocks(pickupBlock)*264;
+  return feetHQ;
+}
+
+function distanceTravelledInFeet(a, b){
+    const distance = Math.abs(a-b)*264;
+    return distance;
+}
+
+function calculatesFarePrice(a, b){
+  const distance = distanceTravelledInFeet(a, b);
+  switch(true){
+    case distance<=400:
+      return 0;
+    case distance<2000:
+      return distance*.02;
+    case distance<2500:
+      return 25;
+    case distance>2500:
+      return 'cannot travel that far'
+  }
+}
